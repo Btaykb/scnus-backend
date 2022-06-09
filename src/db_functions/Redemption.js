cea2e6b8efce3e2142db4ad3b88b654fae34a724
Shashank Acharya <acharya.s@icloud.com>
2022-06-09 17:12:54 +0800

    added redemption crud functions

---
+++ src/db_functions/Redemption.js
@@ -0,0 +1,47 @@
+import { unpackSingleDocument, unpackMultipleDocuments } from '../utils/unpackDocument.js'
+import mongoose from 'mongoose'
+const model = mongoose.model
+const Schema = mongoose.Schema
+
+const schemaTypes = Schema.Types
+
+const RedemptionSchema = Schema({
+	vendor_id: { type: schemaTypes.String, required: true },
+	customer_id: { type: schemaTypes.String, required: true },
+	amount: { type: schemaTypes.Number, required: true },
+	discount: { type: schemaTypes.Number, required: true },
+	time: { type: schemaTypes.String, required: true }
+})
+
+export const RedemptionObject = model('Redemption', RedemptionSchema)
+
+export const createRedemption = async (redemption) => {
+	const httpResponse = new RedemptionObject({
+		...redemption, 
+		time: new Date().toUTCString()
+	}).save()
+		.then(res => {
+			console.log(`New redemption created with id ${res._id}`)
+			return { response: res._id }
+		})
+		.catch(err => {
+			return { error: err.code }
+		})
+	return httpResponse
+}
+
+export const readRedemption = (_id) => {
+	return RedemptionObject.findOne({ _id: _id})
+		.then(unpackSingleDocument)
+		.catch(err => {
+			console.log('Error while getting redemption: DB error')
+		})
+}
+
+export const readRedemptions = (params) => {
+	return RedemptionObject.find(params)
+		.then(unpackMultipleDocuments)
+		.catch(err => {
+			console.log('Error while getting redemptions: DB error')
+		})
+}
