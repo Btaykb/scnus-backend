5df3011eef744e0fc59602fd106b8f0c01d8bfc0
Shashank Acharya <acharya.s@icloud.com>
2022-06-10 11:02:44 +0800

    created tier type

---
+++ src/types/Tier.js
@@ -0,0 +1,39 @@
+import { createModule, gql } from "graphql-modules";
+import { createTier, deleteTier, readTier, readTiers, updateTier } from "../db_functions/Tier.js";
+
+const TierModule = createModule({
+	id: 'tier',
+	typeDefs: gql`
+		type Tier {
+			_id: ID!
+			name: String!
+			tokenReq: Int!
+			discount: Float!
+		}
+
+		type Query {
+			getAllTiers: [Tier!]!
+			getTier(name: String!): Tier
+		}
+
+		type Mutation {
+			createTier(name: String!, tokenReq: Int!, discount: Float!): HTTPResponse
+			updateTier(name: String!, tokenReq: Int!, discount: Float!): HTTPResponse
+			deleteTier(name: ID!): HTTPResponse
+		}
+
+	`,
+	resolvers: {
+		Query: {
+			getAllTiers: () => readTiers(),
+			getTier: (_, args) => readTier(args)
+		},
+		Mutation: {
+			createTier: (_, args) => createTier(args),
+			updateTier: (_, args) => updateTier({name: args.name}, args),
+			deleteTier: (_, args) => deleteTier(args),
+		}
+	}
+})
+
+export default TierModule
