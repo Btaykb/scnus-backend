f44cd9f0cd401162954edfeb48b34f78cbea5dc8
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 09:50:36 +0530

    added tier constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,24 @@
 export const JWT_SIGN_KEY = "scannus"
+
+export const TIERS = [
+	{
+		name: 'No Tier',
+		tokenReq: 0,
+		discount: 0,
+	},
+	{
+		name: 'Bronze',
+		tokenReq: 5,
+		discount: 0.1,
+	},
+	{
+		name: 'Silver',
+		tokenReq: 10,
+		discount: 0.2,
+	},
+	{
+		name: 'Gold',
+		tokenReq: 15,
+		discount: 0.3,
+	},
+]
